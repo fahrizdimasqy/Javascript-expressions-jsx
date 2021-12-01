5a0e85ff8bfea201a7ce018ff35cc5052f915d07
Fahriz Dimasqy <58032217+fahrizdimasqy@users.noreply.github.com>
2021-11-30 22:51:03 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "fahriz";
+const element = <h1>hello, {name}</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
